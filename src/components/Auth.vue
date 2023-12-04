2bcf9febbd9ec1cc867edde4403be51bc4fd57da
clodos <clodos_m92@hotmail.com>
2023-12-04 10:13:13 +0100

    Loginform validation

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -39,26 +39,28 @@
                     </ul>
 
                     <!-- Login Form -->
-                    <form v-show="tab === 'login'">
+                    <vee-form v-show="tab === 'login'" :validation-schema="loginSchema" @submit="login">
                         <!-- Email -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Email</label>
-                            <input type="email"
+                            <vee-field type="email" name="email"
                                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                                 placeholder="Enter Email" />
+                            <ErrorMessage class="text-red-600" name="email" />
                         </div>
                         <!-- Password -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Password</label>
-                            <input type="password"
+                            <vee-field type="password" name="password"
                                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                                 placeholder="Password" />
+                            <ErrorMessage class="text-red-600" name="password" />
                         </div>
                         <button type="submit"
                             class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
                             Submit
                         </button>
-                    </form>
+                    </vee-form>
                     <!-- Registration Form -->
                     <div class="text-white text-center font-bold p-4 rounded mb-4" v-if="reg_show_alert"
                         :class="reg_alert_variant">
@@ -161,6 +163,10 @@ export default {
                 country: 'required|country_excluded:Antarctica',
                 tos: 'tos'
             },
+            loginSchema: {
+                email: 'required|email',
+                password: 'required|min:9|max:100',
+            },
             userData: {
                 country: 'USA'
             },
@@ -190,6 +196,9 @@ export default {
             this.reg_alert_variant = "bg-green-500";
             this.reg_alert_msg = "Success! Your account has been created.";
 
+            console.log(values);
+        },
+        login(values) {
             console.log(values);
         },
 
