488e667a4f48ad9d11aaded4c721d83ea7f89f15
clodos <clodos_m92@hotmail.com>
2023-12-05 16:25:47 +0100

    Uid verwendet zur document abspeicherung

--- src/stores/user.js
+++ src/stores/user.js
@@ -8,15 +8,21 @@ export default defineStore('user', {
   actions: {
     // keine arrow funktion, da sonst this nicht auf das store objekt zeigt
     async register(values) {
-      await auth.createUserWithEmailAndPassword(values.email, values.password)
+      const userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
 
       // User in Firestore speichern
-      await usersCollection.add({
+      // add erstellt ein Document mit einer zufälligen ID
+      // mit doc(userCred.user.uid)und .set() wird die UID des Users als ID des Documents verwendet so können die Daten dem User zugeordnet werden
+      await usersCollection.doc(userCred.user.uid).set({
         name: values.name,
         email: values.email,
         age: values.age,
         userCategory: values.userCategory,
         country: values.country
+      })
+
+      await userCred.user.updateProfile({
+        displayName: values.name
       })
 
       this.userLoggedIn = true
