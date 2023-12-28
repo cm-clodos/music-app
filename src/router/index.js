5b7c87ea29076a401dfabdad96adc833f14e7848
clodos <clodos_m92@hotmail.com>
2023-12-28 17:05:27 +0100

    Redirect, Route Naming

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,35 @@ import Manage from '@/views/ManageView.vue'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home
   },
   {
+    name: 'about',
     path: '/about',
     component: About
   },
   {
-    path: '/manage',
+    name: 'manage',
+    //alias: '/manage',
+    path: '/manage-music',
     component: Manage
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'home' }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
